48774ce42f861324c5acdf03465478b9d6baa270
Simon <Simonteame436@gmail.com>
2025-04-29 10:27:57 +0200

    Opgave Id: D2. Added company properties

---
+++ Company.cs
@@ -0,0 +1,13 @@
+namespace ERP_System;
+
+public class Company
+{
+    public int ComapnyId { get; set; }
+    public string Name { get; set; } = "";
+    public string Street { get; set; } = "";
+    public string StreetNumber { get; set; } = "";
+    public string City { get; set; } = "";
+    public string Country { get; set; } = "";
+    public Currency Currency { get; set; }
+
+}
