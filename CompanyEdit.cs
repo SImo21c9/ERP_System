d3e5424d140219d63d0685ad3f694c64cb11923a
Simon <Simonteame436@gmail.com>
2025-05-02 10:37:52 +0200

    Added SelectBox

--- CompanyEdit.cs
+++ CompanyEdit.cs
@@ -17,6 +17,7 @@ public class CompanyEdit : Screen
     {
         Form<Company> editForm = new();
         editForm.TextBox("Virksomhed", nameof(Company.CompanyName));
+        editForm.SelectBox("Currency", "Currency");
         editForm.AddOption("Currency", "Dansk kroner", Currency.DKK);
         editForm.AddOption("Currency", "Svenske kroner", Currency.SEK);
         editForm.AddOption("Currency", "Euro", Currency.EUR);
