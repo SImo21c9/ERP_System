3d34e2c63d454bb98a96354be4c35856d8bf4306
Superlaban1 <mikkelbach07@gmail.com>
2025-05-12 08:53:53 +0200

    Added CustomerDetailsPage

--- CustomerEditPage.cs
+++ CustomerEditPage.cs
@@ -1,14 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using TECHCOOL.UI;
+namespace ERP_System;
 
-namespace ERP_System
+public class CustomerEdit : Screen
 {
-    internal class CustomerEditPage
+    public override string Title { get; set; } = "Customer";
+    private Customer _customer;
+
+    public CustomerEdit(Customer customer)
     {
+        _customer = customer;
+    }
+
+    protected override void Draw()
+    {
+        Form<Customer> editForm = new();
+        editForm.TextBox("Virksomhed", nameof(Company.CompanyName));
+        editForm.SelectBox("Currency", "Currency");
+        editForm.AddOption("Currency", "Dansk kroner", Currency.DKK);
+        editForm.AddOption("Currency", "Svenske kroner", Currency.SEK);
+        editForm.AddOption("Currency", "Euro", Currency.EUR);
+        editForm.SelectBox("Country", "Country");
+        Country[] topGdpCountries = new[]
+        {
+            Country.UnitedStates,
+            Country.Denmark,
+            Country.China,
+            Country.Germany,
+            Country.Sweden
+        };
+
+        foreach (var country in topGdpCountries)
+        {
+            editForm.AddOption("Country", country.ToString(), country);
+        }
+        //HELLO DELETE ME PLEASE
+        if (editForm.Edit(_customer))
+        {
+            Database.Instance.UpdateCompany(_customer);
+        }
+
+        Display(new CompanyInfo(_customer));
 
     }
 }
- 
