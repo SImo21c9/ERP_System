bf7139a5280ea86e65f3c6e03a71e2c88c374a87
Simon <Simonteame436@gmail.com>
2025-05-12 13:58:32 +0200

    just added some comments, that's it

--- Customer.cs
+++ Customer.cs
@@ -5,6 +5,10 @@ namespace ERP_System;
 
 public class Customer : Person
 {
+    /// <summary>
+    /// Here we made a Hashset which is a type of collections that stores specific data (need to double-check i might have forgotten what it actually does since it's been a while since i wrote it)
+    /// We are storing customerid that cannot be repeated and as you can below that there is an exception that makes sre that a customer id is not repeated 
+    /// </summary>
     private static HashSet<string> existingCostumers = new HashSet<string>();
     private string costumerId;
     public string LastPurchase { get; set; } = DateTime.Now.ToString();
