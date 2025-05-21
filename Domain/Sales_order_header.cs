1f0dbc01a9b933f4e87c191df8155fd1941b4732
Simon <Simonteame436@gmail.com>
2025-05-21 09:13:49 +0200

    Just putting the different things in their place and it looks a little more clean and easy to look at

---
+++ Domain/Sales_order_header.cs
@@ -0,0 +1,12 @@
+namespace ERP_System;
+using TECHCOOL.UI;
+
+public class Sales_order_header
+{
+    public string OrderNumber { get; set; } = "";
+    public string OrderCreatedDate { get; set; } = "";
+    public string OrderCompleteddDate { get; set; } = "";
+    public string CustomerNumber { get; set; } = "";
+    public string OrderStatus { get; set; } = "";
+    public string OrderTotalAmount; // not finished 
+}
--- Sales_order_header.cs
+++
@@ -1,7 +0,0 @@
-namespace ERP_System;
-
-public class Sales_order_header
-{
-    public string OrderNumber { get; set; }
-    public string OrderCreatedDate { get; set; }
-}
