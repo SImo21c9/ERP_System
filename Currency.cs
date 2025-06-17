f854bc2a8880968535d066aabc25bafb2cfdccc1
Simon Teame <simonteame436@gmail.com>
2025-06-17 22:30:50 +0200

    hellow

--- Currency.cs
+++
@@ -1,8 +0,0 @@
-namespace ERP_System;
-using TECHCOOL.UI;
-
-// Valutatyper der kan vælges til virksomheder
-public enum Currency
-{
-    DKK, SEK, USD, EUR, RUB
-}
---
+++ avalonia_api_ipinfo_sideproject/avalonia_api_ipinfo_sideproject/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace avalonia_api_ipinfo_sideproject;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
